3b348239d9494bc0333eb6e9006cb33b4265f38d
Maksim Kazadaev <kazadaevms@yandex.ru>
2023-03-27 17:20:51 +0300

    Correct async_loader.py

--- data_download/async_loader.py
+++ data_download/async_loader.py
@@ -1,12 +1,13 @@
 import aiohttp
 import asyncio
 import aiofiles
-from bs4 import BeautifulSoup
-from json import loads
 import re
-import os
 import requests
-
+import json
+import traceback
+import sys
+from bs4 import BeautifulSoup
+from pathlib import Path
 
 REQUIRED_LIMIT = 10000
 
@@ -34,20 +35,20 @@ def load_data(security: str,
     """
     if filename is None:
         filename = f'data/{engine}-{market}-{security}-{interval}.txt'
-        with open(filename, 'w') as file:
-            start: int = 0
-            while True:
-                url = f'https://iss.moex.com/iss/engines/{engine}/markets/{market}/securities/{security}/candles.json?start={start}&interval={interval}&from={start_date}&till={end_date}'
-                soup = BeautifulSoup(requests.get(url).text, features="lxml")
-                data = loads(soup.body.p.text)['candles']['data']
-                if len(data) == 0:
-                    break
-                for row in data:
-                    str_row = sep.join([str(el) for el in row])
-                    if re.sub(r'[0-9]+.', '', str_row):
-                        continue
-                    file.write(str_row + '\n')
-                start += len(data)
+    with open(filename, 'w') as file:
+        start: int = 0
+        while True:
+            url = f'https://iss.moex.com/iss/engines/{engine}/markets/{market}/securities/{security}/candles.json?start={start}&interval={interval}&from={start_date}&till={end_date}'
+            soup = BeautifulSoup(requests.get(url).text, features="lxml")
+            data = json.loads(soup.body.p.text)['candles']['data']
+            if len(data) == 0:
+                break
+            for row in data:
+                str_row = sep.join([str(el) for el in row])
+                if re.sub(r'[0-9]+.', '', str_row):
+                    continue
+                file.write(str_row + '\n')
+            start += len(data)
 
 
 async def async_load_data(session: aiohttp.ClientSession,
@@ -83,7 +84,7 @@ async def async_load_data(session: aiohttp.ClientSession,
                 url = f'https://iss.moex.com/iss/engines/{engine}/markets/{market}/securities/{security}/candles.json?start={start}&interval={interval}&from={start_date}&till={end_date}'
                 async with session.get(url) as response:
                     soup = BeautifulSoup(await response.text(), features="lxml")
-                    data = loads(soup.body.p.text)['candles']['data']
+                    data = json.loads(soup.body.p.text)['candles']['data']
                     if len(data) == 0:
                         break
                     result_row = ""
@@ -94,14 +95,15 @@ async def async_load_data(session: aiohttp.ClientSession,
                         result_row += str_row + '\n'
                     await file.write(result_row)
                     start += len(data)
-    except Exception as err:
+    except Exception as ex:
         # Maybe this security need to be loaded again
-        print(f'Failed for {security} with {err}')
+        print(f'Error for {security} with {ex}', file=sys.stderr)
+        traceback.print_exc()
     finally:
-        print(f'Ended for {security} with {start} rows')
+        print(f'Downloaded for {security} with {start} rows')
         # Removing files with small history
         if start < REQUIRED_LIMIT:
-            os.remove(filename)
+            Path(filename).unlink()
 
 
 async def async_gather(names: list[str], **kwargs):
@@ -116,9 +118,9 @@ async def async_gather(names: list[str], **kwargs):
         await asyncio.gather(*[async_load_data(session, security=name, **kwargs) for name in names])
 
 
-def make_securities(filename: str = 'securities.txt'):
+def make_securities(filename: Path):
     """
-    Loading names of securities from security statistics
+    Load names of securities from security statistics
 
     :param filename: Path to output file
     :return: None
@@ -128,32 +130,35 @@ def make_securities(filename: str = 'securities.txt'):
     names = list(set(el['secid'] for el in soup.body.document.data.rows.find_all('row')))
     final_names = []
     for name in sorted(names):
+        print(f'Find {name}')
         try:
             url = f'https://iss.moex.com/iss/engines/stock/markets/shares/securities/{name}/candles.json?start=0&interval=1&from=2020-01-01&till=2023-01-01'
             soup = BeautifulSoup(requests.get(url).text, features="lxml")
-            data = loads(soup.body.p.text)['candles']['data']
+            data = json.loads(soup.body.p.text)['candles']['data']
             if len(data) == 0:
                 continue
             final_names.append(name)
-        except Exception:
-            pass
+        except Exception as ex:
+            print(f'Error occurred in make_securities: {ex}')
+            traceback.print_exc()
 
     with open(filename, 'w') as sec:
         sec.writelines([name + '\n' for name in final_names])
 
 
 if __name__ == '__main__':
-    if not os.path.exists('securities.txt'):
-        make_securities()
+    data_folder_path = Path('data/')
+    securities_path = data_folder_path / 'securities.txt'
+    if not data_folder_path.exists():
+        print(f'{data_folder_path} do not exists, create it')
+    if not securities_path.exists():
+        make_securities(securities_path)
 
-    with open('securities.txt', 'r') as sec:
-        names = [name.strip() for name in sec.readlines()]
+    with open(securities_path, 'r') as sec:
+        names = [name.strip() for name in sec.readlines() if name.strip()]
 
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
     try:
-        coroutine = async_gather(names=names, interval=10)
-        loop.run_until_complete(coroutine)
-        loop.close()
+        asyncio.run(async_gather(names=names, interval=1))
     except KeyboardInterrupt:
-        pass
+        print('KeyboardInterrupt')
+    print('Exit')
